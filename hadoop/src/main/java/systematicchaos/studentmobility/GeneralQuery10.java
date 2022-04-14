43b88391332114840d2939e20d0b74735eea7748
Javier Fernández-Bravo Peñuela <fcojavier.fernandezbravo.uclm@gmail.com>
2022-04-14 16:26:45 +0100

    Hadoop - General query 10: Gender proportion of European exchange students.

---
+++ hadoop/src/main/java/systematicchaos/studentmobility/GeneralQuery10.java
@@ -0,0 +1,97 @@
+/**
+ * Student Mobility
+ * 
+ * Javier Fernández-Bravo Peñuela
+ * Universitat Politècnica de València
+ * 
+ * General query 10: Gender proportion of European exchange students.
+ * 
+ * hadoop - GeneralQuery10.java
+ */
+
+package systematicchaos.studentmobility;
+
+import java.util.Iterator;
+
+import static systematicchaos.studentmobility.util.Functions.roundHalfDown;
+
+import java.nio.charset.StandardCharsets;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+
+import systematicchaos.studentmobility.domain.Student;
+import systematicchaos.studentmobility.hadoop.HadoopJobLauncher;
+import systematicchaos.studentmobility.hadoop.HadoopMapper;
+import systematicchaos.studentmobility.hadoop.HadoopReducer;
+import systematicchaos.studentmobility.util.KeyValuePair;
+
+public class GeneralQuery10 {
+	
+	public static class StudentGenderCountMapper extends HadoopMapper<Text, DoubleWritable> {
+		
+		private static final Text GENDER_MALE = new Text(String.valueOf(Student.Gender.MALE.getValue()));
+		private static final Text GENDER_FEMALE = new Text(String.valueOf(Student.Gender.FEMALE.getValue()));
+		private static final DoubleWritable ONE = new DoubleWritable(1.);
+		
+		private static final String HEADER = "HOMEINSTITUTION";
+		
+		@Override
+		public KeyValuePair<Text, DoubleWritable> map(String token) {
+			KeyValuePair<Text, DoubleWritable> mapResult = null;
+			if (!token.startsWith(HEADER)) {
+				Student student = Student.fromString(token);
+				switch (student.getGender()) {
+				case MALE:
+					mapResult = new KeyValuePair<>(GENDER_MALE, ONE);
+					break;
+				case FEMALE:
+					mapResult = new KeyValuePair<>(GENDER_FEMALE, ONE);
+					break;
+				}
+			}
+			return mapResult;
+		}
+	}
+	
+	public static class StudentGenderProportionReducer extends HadoopReducer<Text, DoubleWritable> {
+		
+		private DoubleWritable genderProportion = new DoubleWritable();
+		
+		private static long maleCount = 0;
+		private static long femaleCount = 0;
+		private static long numValues = 0;
+		
+		private static final String GENDER_MALE_VALUE = String.valueOf(Student.Gender.MALE.getValue());
+		private static final String GENDER_FEMALE_VALUE = String.valueOf(Student.Gender.FEMALE.getValue());
+		
+		@Override
+		public DoubleWritable reduce(Text key, Iterator<DoubleWritable> values) {
+			long genderCount = 0;
+			while (values.hasNext()) {
+				genderCount += values.next().get();
+				numValues++;
+			}
+			
+			final String keyGender = new String(key.getBytes(), StandardCharsets.UTF_8);
+			if (GENDER_MALE_VALUE.equals(keyGender)) {
+				maleCount += genderCount;
+				genderCount = maleCount;
+			} else if (GENDER_FEMALE_VALUE.equals(keyGender)) {
+				femaleCount += genderCount;
+				genderCount = femaleCount;
+			}
+			genderProportion.set(roundHalfDown((double)genderCount / numValues));
+			return genderProportion;
+		}
+	}
+	
+	// hadoop jar target/hadoop-1.0.0.jar systematicchaos.studentmobility.GeneralQuery10 ../data/Student_Mobility.csv out/hadoop10-output
+	public static void main(String[] args) throws Exception {
+		final int exitCode = HadoopJobLauncher.launchHadoopJob(
+				GeneralQuery10.class.getName(),
+				StudentGenderCountMapper.class, StudentGenderProportionReducer.class,
+				Text.class, DoubleWritable.class, args);
+		System.exit(exitCode);
+	}
+}
